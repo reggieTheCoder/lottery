0c62a5a4090fb0631ac0bdcb5ab81ecb13185709
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-01-04 19:13:56 -0500

    test to display components

---
+++ src/Lottery.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class Lottery extends Component {
+  render() {
+      return (
+          <div>
+              <h1>Lotto Component</h1>
+          </div>
+      )
+  }
+     
+    }
+
+    export default Lottery;
+    
---
+++ src/LotteryBall.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+class LotteryBall extends Component {
+  render() {
+      return (
+          <div>
+              <h1> Lottery Ball Component</h1>
+          </div>
+      )
+  }
+     
+    }
+
+    export default LotteryBall;
